0754b662643c4085da841016dba01d52fc7f98d5
fcracker79 <fcracker79@gmail.com>
2020-08-24 08:29:30 +0200

    My applicative, Chapter 12.

--- src/Chapter12.hs
+++ src/Chapter12.hs
@@ -5,6 +5,10 @@ class MyFunctor f where
     myfmap :: (a -> b) -> f a -> f b
 
 
+class MyApplicative f where
+    mypure :: a -> f a
+    (<**>) :: f (a -> b) -> f a -> f b
+
 -- This is in the prelude, but if I do the same, as follows...
 -- class MyFunctor2 (f :: * -> *) where
 --    myfmap2 :: (a -> b) -> f a -> f b
@@ -23,9 +27,9 @@ class MyFunctor f where
 instance MyFunctor IO where
     myfmap f o = do {x <- o; return (f x)}
 
-
-newtype MyReader r a = MyReader { runReader :: r -> a }
-
+instance MyApplicative IO where
+    mypure = return
+    (<**>) fio aio = do {f <- fio; a <- aio; return (f a)}
 
 -- Thanks to Haskell prelude! (->) r is the type of all functions that takes an r as an argument.
 instance MyFunctor ((->) r) where
@@ -38,3 +42,8 @@ f :: Int -> Int
 f = (+ 1)
 
 f_mapped = myfmap g f
+
+myGetChars :: Int -> IO String
+myGetChars 0 = return []
+-- myGetChars n = ((<**>) ((<**>) (mypure (:)) getChar) (myGetChars (n - 1)))
+myGetChars n = mypure (:) <**> getChar <**> myGetChars (n - 1)
