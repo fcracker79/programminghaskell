dfb6f9f05e4382117938ce60b51ca44cb0c793f3
fcracker79 <fcracker79@gmail.com>
2020-11-26 08:57:20 +0100

    Playing with loops.

--- src/BookOfMonads/Chapter4.hs
+++ src/BookOfMonads/Chapter4.hs
@@ -4,6 +4,8 @@ import qualified BookOfMonads.Chapter1 as Ch1
 import qualified Text.Read as TR
 import Control.Monad.Loops
 
+
+getChars :: IO String
 getChars = do
     x <- getChar
     if x == '\n' then
@@ -15,8 +17,28 @@ getChars = do
 
 
 getCharsUntilNumber :: Int -> IO String
-
 getCharsUntilNumber n = iterateWhile
     (\x -> case fmap ((>=) n) (TR.readMaybe x::Maybe Int) of
            Nothing -> True
            Just x -> x) getChars
+
+-- whileM :: Monad m => m Bool -> m a -> m [a]
+
+printStuffWhile :: IO [String]
+printStuffWhile = whileM
+    (do
+        print "Continue ?"
+        x <-fmap ((==) 'y') getChar
+        putStrLn ""
+        return x
+    )
+    getChars
+
+
+printStuffIterateWhile :: Int -> IO (Maybe Int)
+printStuffIterateWhile n =
+    iterateWhile
+    (\x -> case x of
+           Nothing -> True
+           Just y -> y < n)
+    $ fmap TR.readMaybe getChars
