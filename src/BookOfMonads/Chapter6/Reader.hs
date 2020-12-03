df72453aedb28d62abff00243e4f5e007b1b9eb3
fcracker79 <fcracker79@gmail.com>
2020-12-03 07:28:23 +0100

    Reader monad.

--- src/BookOfMonads/Chapter6/Reader.hs
+++ src/BookOfMonads/Chapter6/Reader.hs
@@ -11,3 +11,7 @@ instance Functor (MyReader r) where
 instance Applicative (MyReader r) where
     pure x = MyReader (\_ -> x)
     (<*>) (MyReader fr) (MyReader ar) = MyReader (\r -> (fr r) (ar r))
+
+
+instance Monad (MyReader r) where
+    (>>=) (MyReader ra) f = MyReader (\r -> runReader (f (ra r)) r)
