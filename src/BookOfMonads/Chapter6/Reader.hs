1d94af817570311e6442fcb562f0946b32f35c5e
fcracker79 <fcracker79@gmail.com>
2020-12-04 07:25:09 +0100

    withReader.

--- src/BookOfMonads/Chapter6/Reader.hs
+++ src/BookOfMonads/Chapter6/Reader.hs
@@ -15,3 +15,27 @@ instance Applicative (MyReader r) where
 
 instance Monad (MyReader r) where
     (>>=) (MyReader ra) f = MyReader (\r -> runReader (f (ra r)) r)
+
+
+ask :: MyReader r r
+ask = MyReader id
+
+asks :: (r -> a) -> MyReader r a
+asks f = f <$> ask
+
+withReader :: (r -> s) -> MyReader s a -> MyReader r a
+withReader f (MyReader r) = MyReader (r . f)
+
+-- Boh, seems the same
+-- asks2 :: (r -> a) -> MyReader r a
+-- asks2 f = MyReader f
+
+data MyConfig f1 f2 f3 = MyConfig { f1 :: String, f2 :: Int, f3 :: [Int]}
+
+
+getConf :: MyReader (MyConfig f1 f2 f3) String
+getConf = do
+    v1 <- asks f1
+    v2 <- show <$> (asks f2)
+    v3 <- show <$> (asks f3)
+    return (v1 ++ v2 ++ v3)
