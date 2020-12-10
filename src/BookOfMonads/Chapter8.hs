29adb4f3d55c9aec74e2cc28a0fc28255d64fbf9
fcracker79 <fcracker79@gmail.com>
2020-12-10 08:27:15 +0100

    A simple example of exception handling.

--- src/BookOfMonads/Chapter8.hs
+++ src/BookOfMonads/Chapter8.hs
@@ -2,6 +2,7 @@ module BookOfMonads.Chapter8 where
 
 import Data.STRef
 import Control.Monad.ST
+import Control.Exception
 
 sumall :: Int -> Int
 sumall n = if n <= 0 then 0 else runST $ do
@@ -14,3 +15,22 @@ sumall n = if n <= 0 then 0 else runST $ do
 fibonacci :: Int -> Int
 fibonacci n = if n < 2 then 1 else runST $ do
     return ((fibonacci (n - 1)) + (fibonacci (n - 2)))
+
+
+data MyException = AnException | AnotherException deriving(Show)
+instance Exception MyException
+
+throwSomeExceptions :: Int -> IO Int
+throwSomeExceptions x = case x of
+         0 -> throw AnException
+         1 -> throw AnotherException
+         otherwise -> return $ x + 1
+
+catchMyException :: MyException -> IO Int
+catchMyException e = do
+    case e of
+        AnException -> return 1000
+        AnotherException -> return 2000
+
+catchSomeException :: Int -> IO Int
+catchSomeException n = throwSomeExceptions n `catches` [Handler catchMyException]
