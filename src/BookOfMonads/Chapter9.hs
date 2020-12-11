5ba7118523f18fbdd6b112110c87f01e20a393d1
fcracker79 <fcracker79@gmail.com>
2020-12-11 08:19:17 +0100

    Example of continuation with failures using ioError and throwError.

---
+++ src/BookOfMonads/Chapter9.hs
@@ -0,0 +1,44 @@
+module BookOfMonads.Chapter9 where
+
+
+import qualified Control.Exception as CE
+import qualified System.IO.Error as SIE
+import qualified Control.Monad.Except as CME
+import qualified GHC.IO.Exception as GIE
+
+acquire :: IO String
+acquire = do
+          print "acquire"
+          return "myResource"
+
+release :: String -> IO ()
+release x = print $ "release of " ++ x
+
+useWithFailure :: String -> IO ()
+useWithFailure x = do
+                   print $ "Attempting to use " ++ x
+                   SIE.ioError(SIE.userError ("An error occurred while using " ++ x))
+
+
+data MyError = MyError1 | MyError2 deriving(Show)
+useWithThrowError :: String -> IO ()
+useWithThrowError x = do
+                      print $ "Attempting to use" ++ x
+                      CME.throwError $ GIE.IOError Nothing GIE.IllegalOperation "loc" ("an error occurred while using" ++ x) Nothing Nothing
+
+continuationWithPrint :: (String -> IO a) -> IO a
+continuationWithPrint = CE.bracket acquire release
+
+-- "acquire"
+-- "Attempting to use myResource"
+-- "release of myResource"
+-- *** Exception: user error (An error occurred while using myResource)
+continuationWithIOError :: IO ()
+continuationWithIOError = continuationWithPrint useWithFailure
+
+continuationWithSuccess :: IO Int
+continuationWithSuccess = continuationWithPrint (\x -> return $ length x)
+
+
+continuationWithThrowError :: IO ()
+continuationWithThrowError = continuationWithPrint useWithThrowError
