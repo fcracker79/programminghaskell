c626db25c326fea1769f28635696f5bfea831423
fcracker79 <fcracker79@gmail.com>
2020-12-21 09:10:23 +0100

    Just another comment to better remember why my code works.

--- src/BookOfMonads/Chapter11/MaybeTIdentity.hs
+++ src/BookOfMonads/Chapter11/MaybeTIdentity.hs
@@ -4,7 +4,7 @@ newtype Identity a = I a
 newtype MaybeT m a = MaybeT {runMaybeT :: m (Maybe a)}
 
 -- This works because applicative functor is (m ((Maybe a) -> (Maybe b)) ) -> m (Maybe a) -> m (Maybe b)
--- and fmap f is lifted to m through <$> operator.
+-- and fmap f is lifted to m through <$> operator, whereas x is of type "m (Maybe a)".
 instance Monad m => Functor (MaybeT m) where
     fmap f (MaybeT x) = MaybeT $ do fmap f <$> x
 
