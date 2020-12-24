09c58af31106ca8e296297b8d9b79be1e2c1b4f1
fcracker79 <fcracker79@gmail.com>
2020-12-24 08:50:30 +0100

    Digging into usage of monad transformers

--- src/BookOfMonads/Chapter11/MonadReaderExample.hs
+++ src/BookOfMonads/Chapter11/MonadReaderExample.hs
@@ -1,12 +1,29 @@
 module BookOfMonads.Chapter11.MonadReaderExample where
 
-import Control.Monad.Trans.Maybe
+import qualified Control.Monad.Trans.Maybe as M
+import qualified Control.Monad.Trans.Reader as R
+
 
 class Monad m => MonadReader r m | m -> r where
     ask :: m r
     local :: (r -> r) -> m a -> m a
 
 
-instance MonadReader r m => MonadReader r (MaybeT m) where
-    ask = MaybeT $ runMaybeT ask
-    local f m = MaybeT $ runMaybeT (local f m)
+instance Monad m => MonadReader r (R.ReaderT r m) where
+    ask = R.ReaderT return
+    local f m = R.ReaderT $ R.runReaderT m . f
+
+
+instance MonadReader r m => MonadReader r (M.MaybeT m) where
+    ask = M.MaybeT $ M.runMaybeT ask -- (1)
+    local f m = M.MaybeT $ M.runMaybeT (local f m)
+
+
+-- This function goes into a loop, because of (1) is a recursive form 
+-- and NOT a delegation of the MonadReader r m (to be verified with Functional Programming guys)
+testMyAsk :: M.MaybeT (R.ReaderT String IO) ()
+testMyAsk = do
+    v <- ask
+
+    -- how to deal with IO?
+    return ()
