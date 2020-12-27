de46a3c3f0e62996dd0304bdf57dbb8d5b9989f6
fcracker79 <fcracker79@gmail.com>
2020-12-27 17:44:50 +0100

    Seems to work

--- src/BookOfMonads/Chapter11/JSONFileSearch.hs
+++ src/BookOfMonads/Chapter11/JSONFileSearch.hs
@@ -11,7 +11,7 @@ import qualified Control.Monad.Trans.State as S
 import qualified Data.Aeson as J
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Text as T
-
+import qualified Data.ByteString.Lazy as L
 
 mRecLookup :: [String] -> Maybe J.Value -> Maybe J.Value
 mRecLookup [] _ = Nothing
@@ -32,15 +32,14 @@ split s c =  case dropWhile (== c) s of
                       s' -> w : split s'' c
                             where (w, s'') = break (== c) s'
 
-recLookup :: String -> J.Value -> Maybe J.Value
-recLookup s o = mRecLookup (split s '.') (Just o)
 
-
--- Where is withReader ?
-searchJSONElement :: R.ReaderT JSONSearchCriteria (M.MaybeT IO) String
+searchJSONElement :: R.ReaderT JSONSearchCriteria (M.MaybeT IO) (Maybe J.Value)
 searchJSONElement = do
     searchCriteria <- ask
     let _jsonPath = jsonPath searchCriteria
     let _fileName = fileName searchCriteria
-
-    return ""
+    jsonContents <- (MIO.liftIO . L.readFile) _fileName
+    return $ mRecLookup (split _jsonPath '.') (J.decode jsonContents)
+    --case mRecLookup (split _jsonPath '.') (J.decode jsonContents) of
+    --    Just s -> return s
+    --    _ -> return Nothing
