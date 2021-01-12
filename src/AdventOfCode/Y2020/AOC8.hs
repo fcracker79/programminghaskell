cdf23333de6a6d852d5b56cb9ef99b6ad80dfc58
fcracker79 <fcracker79@gmail.com>
2021-01-12 07:49:18 +0100

    Added Reader for program instructions in advent of code 8.

--- src/AdventOfCode/Y2020/AOC8.hs
+++ src/AdventOfCode/Y2020/AOC8.hs
@@ -4,14 +4,17 @@ module AdventOfCode.Y2020.AOC8(
     accumulatorValue
 ) where
 
+-- Without this, MaybeT does not implement MonadReader
+import Control.Monad.Reader(asks, runReader)
+import Control.Monad.State.Class(get, put, state)
 import qualified Misc.MyParser as P
 import qualified Control.Monad.Trans.Maybe as M
 import Control.Applicative((<|>), many)
 import qualified Data.Set as Set
 import Data.Maybe(fromMaybe)
-import Control.Monad.Trans.State.Lazy(evalState)
-import Control.Monad.State.Lazy (get, put, state, State)
+import Control.Monad.Trans.State.Lazy(evalStateT, StateT)
 import Control.Monad(guard)
+import Control.Monad.Trans.Reader (Reader)
 
 data Instruction = JMP | NOP | ACC deriving(Show, Eq)
 data InstructionLine = InstructionLine { instruction :: Instruction, argument :: Int } deriving(Show, Eq)
@@ -50,18 +53,20 @@ parseInstructions = do
 
 data ExecutionState = ExecutionState { 
         executedSteps :: Set.Set Int, 
-        program :: [InstructionLine], 
         value :: Int,
         step :: Int
     }
 
-executeInstruction :: M.MaybeT (State ExecutionState) Int
+newtype Program = Program { instructions :: [InstructionLine] }
+
+executeInstruction :: M.MaybeT (StateT ExecutionState (Reader Program)) Int
 executeInstruction = do
+    program <- asks instructions
     state <- get
     let currentValue = value state
     let currentStep =  step state
     guard $ not $ Set.member currentStep $ executedSteps state
-    let InstructionLine instruction argument = program state !! currentStep
+    let InstructionLine instruction argument = program !! currentStep
     let nextStep = currentStep + case instruction of
                                     JMP -> argument
                                     _ -> 1
@@ -70,23 +75,23 @@ executeInstruction = do
                                     _ -> 0
     put ExecutionState {
         executedSteps = Set.insert currentStep $ executedSteps state,
-        program = program state,
         value = nextValue,
         step = nextStep
     }
     return nextValue
 
-executeProgram :: M.MaybeT (State ExecutionState) Int
+executeProgram :: M.MaybeT (StateT ExecutionState (Reader Program)) Int
 executeProgram = do
     many executeInstruction
     value <$> get
 
 
+-- runReader $ (runStateT (M.runMaybeT executeProgram) initialState) program
 accumulatorValue :: [InstructionLine] -> Int
-accumulatorValue program = fromMaybe 0 $ evalState (M.runMaybeT executeProgram) initialState
+accumulatorValue instructions = fromMaybe 0 $ runReader (evalStateT (M.runMaybeT executeProgram) initialState) program
         where initialState = ExecutionState {
                 executedSteps = Set.empty,
-                program = program,
                 value = 0,
                 step = 1
-            }
+              }
+              program = Program { instructions = instructions }
