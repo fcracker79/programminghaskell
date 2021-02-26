e0cc45286dd33599da60e85c51b82993de58448c
fcracker79 <fcracker79@gmail.com>
2021-02-26 12:02:04 +0100

    Morra

--- src/Morra.hs
+++ src/Morra.hs
@@ -1,100 +1,117 @@
-module Morra(playWithComputer, MorraState, initialState) where
+module Morra(playWithComputer, playWithFriend, MorraState, initialState) where
 
 
 import Data.Map(Map)
-import Control.Monad.Trans.State.Lazy(StateT, get, put)
+import Control.Monad.Trans.State.Lazy(StateT, get, put, evalStateT)
 import System.Random ( newStdGen, Random(randomR), StdGen )
 import Control.Monad.IO.Class ( MonadIO(liftIO) )
 import Data.Maybe ( fromMaybe )
 import Text.Read(readMaybe)
 import qualified Data.Map as Map
 import Control.Monad (guard)
+import Control.Applicative((<|>))
+import Text.Printf ( printf )
 
-data MorraState = MorraState { computerIsEven :: Bool, playerScore :: Int, computerScore :: Int, latestPlayerChoices :: [Int], recordedPlayerChoices :: Map (Int, Int) Int }
+data MorraState = MorraState { player1IsEven :: Bool, player1Score :: Int, player2Score :: Int, latestPlayer2Choices :: [Int], recordedPlayer2Choices :: Map (Int, Int) Int }
+data Player = Player1 | Player2
+type PlayersNames = (String, String)
+type PlayerAction = StateT MorraState IO Int
+
 
 initialState :: IO MorraState
 initialState = do
     g <- newStdGen
-    let (computerChoice, _) = randomR (1, 10) g :: (Int, StdGen)
+    let (player1Choice, _) = randomR (1, 10) g :: (Int, StdGen)
     return $ MorraState {
-        computerIsEven = even computerChoice,
-        playerScore = 0,
-        computerScore = 0,
-        latestPlayerChoices = [],
-        recordedPlayerChoices = Map.empty 
+        player1IsEven = even player1Choice,
+        player1Score = 0,
+        player2Score = 0,
+        latestPlayer2Choices = [],
+        recordedPlayer2Choices = Map.empty 
 }
 
 msg :: Show a => a -> StateT MorraState IO ()
 msg = liftIO . print
 
-
-getPlayerChoice :: StateT MorraState IO Int
-getPlayerChoice = do
-    msg "Take a choice"
+getPlayerChoice :: String -> PlayerAction
+getPlayerChoice playerName = do
+    msg $ "Take a choice, " ++ playerName
     playerChoiceString <- liftIO getLine
-    let playerChoice = readMaybe playerChoiceString :: (Maybe Int)
-    fromMaybe <$> getPlayerChoice <*> pure playerChoice 
+    let choice = playerChoice playerChoiceString
+    maybe (getPlayerChoice playerName) pure choice 
+    where playerChoice s = (readMaybe s :: (Maybe Int)) >>= \x -> if x >=1 && x <= 10 then Just x else Nothing
 
 
-guessComputerChoice :: StateT MorraState IO (Maybe Int)
-guessComputerChoice = do
-    state <- get 
-    let latestChoices = latestPlayerChoices state
-    guard $ length (latestPlayerChoices state) >= 2
-    let key = (latestChoices !! (length latestChoices - 2), latestChoices !! (length latestChoices - 1)) 
-    return $ Map.lookup key (recordedPlayerChoices state)
-
-
-getRandomComputerChoice :: StateT MorraState IO Int
-getRandomComputerChoice = do
-    g <- liftIO newStdGen
-    let (computerChoice, _) = randomR (1, 10) g :: (Int, StdGen)
-    return computerChoice
-
-getComputerChoice :: StateT MorraState IO Int
-getComputerChoice = fromMaybe <$> getRandomComputerChoice <*> guessComputerChoice
-
-
-nextPlayerChoices :: Int -> [Int] -> [Int]
-nextPlayerChoices d [a, b, c] = [b, c, d]
-nextPlayerChoices a v = a:v
-
-
-recordNewPlayerChoices :: [Int] -> Map (Int, Int) Int -> Map (Int, Int) Int
-recordNewPlayerChoices [a, b, c] m = Map.insert (a, b) c m
-recordNewPlayerChoices _ m = m
-
-
-computerWins :: Int -> StateT MorraState IO ()
-computerWins i = increaseWinner i 0 1
-
-playerWins :: Int -> StateT MorraState IO ()
-playerWins i = increaseWinner i 1 0
-
-
-increaseWinner :: Int -> Int -> Int -> StateT MorraState IO ()
-increaseWinner i playerDelta computerDelta = do
+getComputerChoice :: PlayerAction
+getComputerChoice = do
     state <- get
-    let newLatestPlayerChoices = nextPlayerChoices i $ latestPlayerChoices state
-    put MorraState { 
-        computerIsEven = computerIsEven state,
-        playerScore = playerScore state + playerDelta,
-        computerScore = computerScore state + computerDelta,
-        latestPlayerChoices = newLatestPlayerChoices,
-        recordedPlayerChoices = recordNewPlayerChoices newLatestPlayerChoices $ recordedPlayerChoices state 
-    }
+    msg $ recordedPlayer2Choices state
+    liftIO $ fromMaybe getRandomComputerChoice $ guessComputerChoice state
+    where guessComputerChoice state = do
+              let latestChoices = latestPlayer2Choices state
+              guard $ length (latestPlayer2Choices state) >= 2
+              let key = (latestChoices !! (length latestChoices - 2), latestChoices !! (length latestChoices - 1)) 
+              let maybeWinningValue = (\i -> if player1IsEven state then i else (i + 1) `mod` 10) <$> Map.lookup key (recordedPlayer2Choices state)
+              pure <$> maybeWinningValue
+          getRandomComputerChoice = do fmap fst $ randomR (1 :: Int, 10) <$> newStdGen
 
-findWinner :: StateT MorraState IO (Maybe String)
+increaseWinner :: Int -> Int -> Int -> String -> StateT MorraState IO ()
+increaseWinner player1Delta player2Delta lastChoice playerName = do
+    msg $ "Player " ++ playerName ++ " wins"
+    state <- get
+    let newLatestPlayerChoices = nextPlayerChoices lastChoice $ latestPlayer2Choices state
+    put MorraState { 
+        player1IsEven = player1IsEven state,
+        player1Score = player1Score state + player1Delta,
+        player2Score = player2Score state + player2Delta,
+        latestPlayer2Choices = newLatestPlayerChoices,
+        recordedPlayer2Choices = recordNewPlayerChoices newLatestPlayerChoices $ recordedPlayer2Choices state 
+    }
+    where nextPlayerChoices d [a, b, c] = [b, c, d]
+          nextPlayerChoices a v = a:v
+          recordNewPlayerChoices [a, b, c] m = Map.insert (a, b) c m
+          recordNewPlayerChoices _ m = m
+
+findWinner :: StateT MorraState IO (Maybe Player)
 findWinner = do
     state <- get
-    return $ if playerScore state >= 10 then Just "Congratulations, player!" else if computerScore state >= 10 then Just "Sorry, computer wins" else Nothing
+    (guard (player1Score state >= 10) >> return (Just Player1)) <|>
+        (guard (player2Score state >= 10) >> return (Just Player2)) <|>
+        return Nothing
 
-playWithComputer :: StateT MorraState IO String
-playWithComputer = do
+playMorraM :: PlayersNames -> PlayerAction -> PlayerAction -> StateT MorraState IO Player
+playMorraM playersNames player1Choice player2Choice = do
+    let (p1Name, p2Name) = playersNames
     state <- get
-    computerChoice <- getComputerChoice
-    playerChoice <- getPlayerChoice
-    let isEven = even $ playerChoice + computerChoice
-    if isEven == computerIsEven state then computerWins playerChoice else playerWins playerChoice
-    state <- get
-    fromMaybe <$> playWithComputer <*> findWinner
+    p1Choice <- player1Choice
+    p2Choice <- player2Choice
+    liftIO $ printf "%s chooses %d\n%s chooses %d\n" p1Name p1Choice p2Name p2Choice
+    let isEven = even $ p1Choice + p2Choice
+    if isEven == player1IsEven state then player1Wins p2Choice p1Name else player2Wins p2Choice p2Name
+    maybeWinner <- findWinner
+    case maybeWinner of
+        Nothing -> playMorraM playersNames player1Choice player2Choice
+        Just Player1 -> do
+            msg $ fst playersNames ++ " is the final winner!"
+            return Player2
+        Just Player2 -> do
+            msg $ snd playersNames ++ " is the final winner!"
+            return Player2
+    where player1Wins = increaseWinner 1 0
+          player2Wins = increaseWinner 0 1
+
+playMorra :: PlayersNames -> PlayerAction -> PlayerAction -> IO Player
+playMorra playersNames p1Choice p2Choice = do
+    is <- initialState
+    let p1Even = player1IsEven is
+    let p1Role = if p1Even then "even" else "odd" :: String
+    let p2Role = if p1Even then "odd" else "even" :: String
+    
+    printf "%s plays %s\n%s plays %s\n" (fst playersNames) p1Role (snd playersNames) p2Role
+    evalStateT (playMorraM playersNames p1Choice p2Choice) is
+
+playWithComputer :: IO Player
+playWithComputer = playMorra ("Computer", "Player 1") getComputerChoice $ getPlayerChoice "Player 1" 
+
+playWithFriend :: IO Player
+playWithFriend = playMorra ("Player 1", "Player 2") (getPlayerChoice "Player 1") (getPlayerChoice "Player 2")
