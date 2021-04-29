d8f8e7339ac7eaa303f9453dd02698a10756dd01
fcracker79 <fcracker79@gmail.com>
2021-04-29 07:54:57 +0200

    Add element tests in FingerTree working. Thanks to @paolino

--- src/HaskellInDepth/CH1/PaolinoExercises/OrderedList.hs
+++ src/HaskellInDepth/CH1/PaolinoExercises/OrderedList.hs
@@ -8,10 +8,10 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-
 module HaskellInDepth.CH1.PaolinoExercises.OrderedList(insert, remove) where
 
-import Data.FingerTree ( FingerTree, Measured(..) )
+import Data.Monoid((<>))
+import Data.FingerTree ( FingerTree, Measured(..), (<|), (|>), split )
 import Protolude
     ( Eq,
       Num,
@@ -21,15 +21,25 @@ import Protolude
       Maybe(Just),
       (.),
       notImplemented,
-      Last(Last) )
+      Last(Last, getLast),
+      (>),
+      Maybe(..),
+      Bool(..) )
 
 
 
 type Ordered a = FingerTree (Last a) a
 
+
+splitPoint :: (Ord a, Measured (Last a) a) => a -> Last a -> Bool 
+splitPoint x (Last Nothing) = False 
+splitPoint x (Last (Just y)) = y > x
+
 -- extract 'k' highest scoring Song , O (1) * k
 insert :: (Measured (Last a) a, Ord a) => a -> Ordered a -> Ordered a
-insert x xs = notImplemented 
-
+insert x xs = (left |> x) <> right 
+  where 
+    (left, right) = split f xs
+    f = splitPoint x
 remove :: (Measured (Last a) a, Ord a) => a -> Ordered a -> Ordered a
 remove x xs = notImplemented 
