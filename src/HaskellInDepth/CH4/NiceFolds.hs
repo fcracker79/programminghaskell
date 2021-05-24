aa7541dbfef649ef8b0ff9d67e9f5781d35e589b
fcracker79 <fcracker79@gmail.com>
2021-05-24 08:20:54 +0200

    Nice folds composition

--- src/HaskellInDepth/CH4/NiceFolds.hs
+++ src/HaskellInDepth/CH4/NiceFolds.hs
@@ -3,8 +3,10 @@ module HaskellInDepth.CH4.NiceFolds where
 
 
 import Data.Monoid
-import Prelude hiding(sum)
-import Data.Foldable
+import qualified Prelude as P
+import Prelude hiding(sum, product, length)
+import Data.Foldable hiding(sum, fold, product, length)
+import Control.Applicative
 
 
 -- Simplest case
@@ -19,6 +21,8 @@ fold (Fold tally summarize) is = summarize $ reduce $ fmap tally is
 sum :: Num n => Fold n n
 sum = Fold Sum getSum
 
+product :: Num n => Fold n n
+product = Fold Product getProduct
 
 -- More interesting example: Average
 
@@ -31,8 +35,8 @@ instance Num a => Monoid (Average a) where
     mempty = Average 0 0
 
 
-average :: Fractional a => Fold a a
-average = Fold tally summarize
+averageOLD :: Fractional a => Fold a a
+averageOLD = Fold tally summarize
     where 
         tally x = Average x 1
         summarize (Average numerator denominator) = numerator / fromIntegral denominator
@@ -82,8 +86,8 @@ ema = Fold tally summarize
 -- Combining folds
 
 -- Tuples are monoids, provided that their elements are as well.
-combine :: Fold i a -> Fold i b -> Fold i (a, b)
-combine (Fold tallyL summarizeL) (Fold tallyR summarizeR) = 
+combineOld :: Fold i a -> Fold i b -> Fold i (a, b)
+combineOld (Fold tallyL summarizeL) (Fold tallyR summarizeR) = 
     Fold tally summarize
         where 
             tally x = (tallyL x, tallyR x)
@@ -103,7 +107,93 @@ instance Functor (Fold i) where
 --             -- ...then the first summarize provides a FUNCTION, whereas the second one produces its argument
 --             summarize (mF, mX) = summarizeF mF (summarizeX mX)
 
+-- instance Applicative (Fold i) where
+--     pure o = Fold (const ()) (const o)
+--     (<*>) f i = apply (combine f i)
+--         where apply (Fold i f) = Fold i ((\(ff, oo) -> ff oo) . f)
+
+-- Pair
+
+data Pair a b = P !a !b
+
+
+instance (Semigroup a, Semigroup b) => Semigroup (Pair a b) where
+    (<>) (P a1 b1) (P a2 b2) = P (a1 <> a2) (b1 <> b2)
+
+instance (Monoid a, Monoid b) => Monoid (Pair a b) where
+    mempty = P mempty mempty
+
+
+-- Applicative using above defined Pair
 instance Applicative (Fold i) where
     pure o = Fold (const ()) (const o)
-    (<*>) f i = apply (combine f i)
-        where apply (Fold i f) = Fold i ((\(ff, oo) -> ff oo) . f)
+    Fold tallyF summarizeF <*> Fold tallyX summarizeX = Fold tally summarize 
+        where 
+            tally i = P (tallyF i) (tallyX i)
+            summarize (P oF oX) = summarizeF oF (summarizeX oX) 
+
+
+combine :: Fold i a -> Fold i b -> Fold i (a, b)
+combine = liftA2 (,)
+
+-- Using applicative interface
+
+
+sumAndProduct :: Num a => [a] -> (a, a)
+sumAndProduct = fold ((,) <$> sum <*> product)
+
+-- This is BAD as, due to the product, the array elements cannot be garbage collected while proceeding with the sum.
+badSumAndProduct :: (Foldable t, Num b) => t b -> (b, b)
+badSumAndProduct xs = (P.sum xs, P.product xs)
+
+
+-- Numerical instance
+instance Num b => Num (Fold a b) where
+    fromInteger n = pure (fromInteger n)
+    negate = fmap negate 
+    abs = fmap abs
+    signum = fmap signum
+    (+) = liftA2 (+)
+    (*) = liftA2 (*)
+    (-) = liftA2 (-)
+
+instance Fractional b => Fractional (Fold a b) where
+    fromRational n = pure (fromRational n)
+    recip = fmap recip
+    (/) = liftA2 (/)
+
+instance Floating b => Floating (Fold a b) where
+    pi = pure pi
+    exp = fmap exp
+    sqrt = fmap sqrt
+    log = fmap log
+    sin = fmap sin
+    cos = fmap cos
+    tan = fmap tan
+    asin = fmap asin
+    acos = fmap acos
+    atan = fmap atan
+    sinh = fmap sinh
+    cosh = fmap cosh
+    tanh = fmap tanh
+    acosh = fmap acosh
+    asinh = fmap asinh
+    atanh = fmap atanh
+
+    (**) = liftA2 (**)
+    logBase = liftA2 logBase
+
+average :: Fold Double Double
+average = sum / length
+
+compositionOfFoldExample :: Double
+compositionOfFoldExample = fold (cos average ^ 2 + sin average ^ 2) [1..10]
+
+-- We use Num instance of Fold
+wat :: Integer
+wat = fold 99 [1..10]
+
+
+standardDeviation :: Fold Double Double
+standardDeviation = sqrt (sumOfSquares / length - (sum / length) ^ 2)
+    where sumOfSquares = Fold (Sum . (^ 2)) getSum
